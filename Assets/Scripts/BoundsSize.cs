169c1fb54dfacf380bb60f81f008b95e1cdb4283
Desktop <filip@weareyord.com>
2020-03-10 07:20:52 +0100

    init

---
+++ Assets/Scripts/BoundsSize.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsSize : MonoBehaviour
+{
+    private Renderer rend = null;
+    public GameObject prefab;
+
+    private GameObject top;
+    private GameObject bottom;
+    private GameObject left;
+    private GameObject right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        top = Instantiate(prefab);
+        bottom = Instantiate(prefab);
+        left = Instantiate(prefab);
+        right = Instantiate(prefab);
+    }
+
+    private void Update()
+    {
+        var position = rend.transform.position;
+        position.y += rend.bounds.size.y;
+
+        top.transform.position = position;
+
+
+        position = rend.transform.position;
+
+        var rotation = Vector3.zero;
+        rotation.z = 180;
+
+        bottom.transform.position = position;
+        bottom.transform.eulerAngles = rotation;
+
+
+        position = rend.transform.position;
+        position.x -= rend.bounds.extents.x;
+        position.y += rend.bounds.extents.y;
+
+        rotation = Vector3.zero;
+        rotation.z = 90;
+
+        left.transform.position = position;
+        left.transform.eulerAngles = rotation;
+
+
+        position = rend.transform.position;
+        position.x += rend.bounds.extents.x;
+        position.y += rend.bounds.extents.y;
+
+        rotation = Vector3.zero;
+        rotation.z = -90;
+
+        right.transform.position = position;
+        right.transform.eulerAngles = rotation;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0f, 1f, 0f, 0.3f);
+
+        Gizmos.DrawCube(rend.bounds.center, rend.bounds.size);
+    }
+}
---
+++ Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FPSDisplay : MonoBehaviour
+{
+    public float hudRefreshRate = 1f;
+    public TextMeshPro textMesh;
+
+    private float deltaTime = 0.0f;
+    private float timer;
+
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+        if (Time.unscaledTime > timer)
+        {
+            
+            float fps = 1.0f / deltaTime;
+
+            string text = string.Format("{0:0.} fps", fps);
+
+            textMesh.text = text;
+
+            timer = Time.unscaledTime + hudRefreshRate;
+        }
+    }
+}
